a88bc0fee96031387bafac38a4e66c1743d4d3d9
Ahmad Mehrabi <46777100+Ahmadmehrabi@users.noreply.github.com>
2024-08-07 15:21:00 +0330

    Add files via upload

---
+++ components/info.js
@@ -0,0 +1,11 @@
+
+import { StyleSheet, View,Image,Text,ScrollView} from 'react-native';
+
+
+export default function InfoScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Information!</Text>
+    </View>
+  );
+}
