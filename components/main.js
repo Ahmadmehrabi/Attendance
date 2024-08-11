59c15cbe7e86181ca41725d0d79da604440485b8
AhmadMehrabi <Ahmad.mehraby@gmail.com>
2024-08-11 19:36:33 +0330

    Update scan

--- components/main.js
+++ components/main.js
@@ -1,13 +1,17 @@
 import { CameraView, CameraType, useCameraPermissions } from 'expo-camera';
 import { useState } from 'react';
-import { Button, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { StyleSheet, Text, View} from 'react-native';
+
+
+import Button from '../components/Button';
 
 export default function MainScreen() {
-  const [facing, setFacing] = useState('back');
+  const [onsuccess, setOnsuccess] = useState(false);
   const [permission, requestPermission] = useCameraPermissions();
 
   const handleCodeScanned = (data) => {
-    alert(data);
+    alert(data.type);
+    setOnsuccess(true)
   }
 
   if (!permission) {
@@ -16,29 +20,52 @@ export default function MainScreen() {
   }
 
   if (!permission.granted) {
-    // Camera permissions are not granted yet.
+    
     return (
       <View style={styles.container}>
-        <Text style={styles.message}>We need your permission to show the camera</Text>
+        <Text style={styles.message}>We need your permission to scan the QR code</Text>
         <Button onPress={requestPermission} title="grant permission" />
       </View>
     );
   }
 
-  function toggleCameraFacing() {
-    setFacing(current => (current === 'back' ? 'front' : 'back'));
-  }
 
   return (
+    <>
+    {onsuccess === false &&
     <View style={styles.container}>
       <CameraView style={styles.camera} barcodeScannerSettings={{ barCodeTypes: ['qr'] }} onBarcodeScanned={handleCodeScanned}>
 
       </CameraView>
 
       <View style={styles.info}>
-      <Text>Salam</Text>
+      <Text style={styles.message}>Student info after scan</Text>
       </View>
     </View>
+    }
+    {onsuccess===true &&
+        <View style={styles.container}>
+        <View style={styles.camera} >
+  
+        </View>
+
+        
+  
+        <View style={styles.info}>
+        <Button style={[styles.button,{width:'90%'},{height:'35%'}]} 
+                            activeOpacity={0.4}
+                            onPress={()=>{}}
+                            disabled={false}>
+
+                    
+                    <Text style={styles.text} >Next</Text>
+                   </Button>
+
+        </View>
+      </View>
+    }
+
+    </>
   );
 }
 
@@ -50,15 +77,24 @@ const styles = StyleSheet.create({
   message: {
     textAlign: 'center',
     paddingBottom: 10,
+    fontSize: 20,
+    fontWeight: 'bold',
+    color: 'black',
+    justifyContent: 'center',
+
   },
   camera: {
-    flex: 1,
+    flex: 4,
   },
   info: {
     flex: 1,
-    flexDirection: 'row',
-    backgroundColor: 'green',
-    margin: 64,
+    //flexDirection: 'row',
+    //backgroundColor: 'khaki',
+    margin: 5,
+    borderRadius:20,
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding:10,
   },
 
   text: {
@@ -66,9 +102,25 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
     color: 'white',
   },
+  button: {
+    borderRadius: 25,
+    alignItems: 'center',
+    justifyContent: 'center',
+    flexDirection: 'row',
+    backgroundColor: "#f194ff",
+    //padding: 10,
+    borderWidth: 3,
+    margin:5,
+    borderColor: "white",        
+},
 });
 
 
 //          <TouchableOpacity style={styles.button} onPress={toggleCameraFacing}>
 //            <Text style={styles.text}>Flip Camera</Text>
 //          </TouchableOpacity>
+
+//<Button title='Next' 
+//color="#f194ff"
+//onPress={()=>{}} >
+//</Button>
